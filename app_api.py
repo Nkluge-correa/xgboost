5485dfcb590ecdbe4bc4fba37923eaa3a16693bc
Nicholas <nicholas@airespucrs.org>
2023-03-05 15:02:43 +0100

    final api version

--- app_api.py
+++ app_api.py
@@ -1,13 +1,16 @@
+import uvicorn
 import numpy as np
 import pandas as pd
 import xgboost as xgb
 from typing import List
+from fastapi import FastAPI
 from pydantic import BaseModel
 from datetime import timedelta
-from xgboost import XGBRegressor
-from fastapi import FastAPI, HTTPException
+from sklearn.preprocessing import StandardScaler
 
 app = FastAPI()
+
+scaler = StandardScaler()
 
 """
 To run this script, you will need to install
@@ -17,7 +20,9 @@ by the `requirements.txt` from this repository.
 """
 
 FEATURES = ['difference_1', 'difference_2', 'difference_3',
-            'difference_4', 'difference_5', 'difference_6', 'difference_7',
+            'difference_4', 'difference_5', 'difference_6',
+            'difference_7', 'moving_average_week',
+            'moving_average_two_weeks', 'difference_year',
             'day_of_week', 'day_of_year', 'quarter', 'month', 'year']
 
 TARGET = ['sales']
@@ -26,29 +31,41 @@ TARGET = ['sales']
 def create_dataframe(list1, list2):
     """
     Creates a dataframe form two arrays.
-    It alsos strips outliers off.
     """
     data = {'dates': list1, 'sales': list2}
+
     df = pd.DataFrame(data)
-    df.loc[df['sales'] > df.sales.mean() + (df.sales.std() * 3),
-           'sales'] = df.sales.mean() + (df.sales.std() * 3)
+
     return df
 
 
 def create_sales_features(df):
     """
     Creates a copy of the input df, and
-    clalculates the difference in sales
-    up to 7 days back
+    calculates the difference in sales 
+    7 days back, and one year back. The 
+    second line puts all values higher than 
+    mean * (std * 3) equal to this value. It also
+    creates features to log the moving average
+    of sales in a one-week and two-week window.
     """
     df = df.copy()
+
+    df.loc[df['sales'] > df.sales.mean() + (df.sales.std() * 3),
+           'sales'] = df.sales.mean() + (df.sales.std() * 3)
 
     previous = df.sales.shift(1)
     df['difference_1'] = df.sales - previous
 
+    df['moving_average_week'] = df.sales.rolling(window=7).mean()
+
+    df['moving_average_two_weeks'] = df.sales.rolling(window=14).mean()
+
     for i in range(1, 7):
         column = 'difference_' + str(i+1)
         df[column] = df['difference_1'].shift(i)
+
+    df['difference_year'] = df.sales - df.sales.shift(366)
 
     df = df.dropna()
 
@@ -80,16 +97,42 @@ def create_time_features(df):
     return df
 
 
+def scale_dataset(df):
+    """
+    This fuction is used to scale all feature values.
+    It transforms each feature so that it has a 
+    mean of 0 and a standard deviation of 1. 
+    """
+
+    df = df.copy()
+
+    features = df.drop(['sales'], axis=1)
+    target = df[['sales']]
+
+    scaler.fit(features)
+
+    features = pd.DataFrame(
+        scaler.transform(features),
+        columns=features.columns,
+        index=features.index)
+
+    df = pd.concat([target, features], axis=1)
+
+    return df
+
+
 def train_model(df):
     """
     Trains an XGBoost Regression Model.
     """
 
-    model = XGBRegressor(n_estimators=400, booster='gbtree',
-                         learning_rate=0.05)
+    model = xgb.XGBRegressor(n_estimators=1000, booster='gbtree',
+                             max_depth=2,
+                             learning_rate=0.1)
 
     train_df = create_sales_features(df)
     train_df = create_time_features(train_df)
+    train_df = scale_dataset(train_df)
 
     x_features = train_df[FEATURES]
     y_target = train_df[TARGET]
@@ -97,6 +140,7 @@ def train_model(df):
     model.fit(x_features, y_target,
               eval_set=[(x_features, y_target)],
               verbose=100)
+
     return model
 
 
@@ -109,8 +153,9 @@ def generate_forecast(model, df, ahead):
     features dataframe with one additional day (a future day),
     and uses the lag features to predict the value of this day.
     In the end, we append this day on the bottom of the seed 
-    `df` and repeat. The function returns only the future
-    predictions.
+    `df` and repeat. The function returns the future
+    predictions, and statistical information about the 
+    distribution of sales.
     """
 
     df = df.set_index('dates')
@@ -146,12 +191,22 @@ def api_call(list1, list2, ahead):
     generates two lists ([dates], [sales]).
     """
     df = create_dataframe(list1, list2)
+
+    statistics = dict(mean=df.sales.mean(),
+                      minimum=df.sales.min(),
+                      maximum=df.sales.max(),
+                      variance=df.sales.var(),
+                      std=df.sales.std(),)
+
     model = train_model(df)
+
     forecast = generate_forecast(model, df, abs(ahead))
-    return list(forecast.index.strftime("%Y-%m-%d")), list(forecast.sales)
+
+    return list(forecast.index.strftime("%Y-%m-%d")), list(forecast.sales), statistics
 
 
 class InputData(BaseModel):
+    product: str
     dates: List[str]
     sales: List[float]
     ahead: int
@@ -164,8 +219,10 @@ async def root():
 
 @app.post("/predict")
 async def predict(data: InputData):
-    days, sales = api_call(data.dates, data.sales, data.ahead)
-    return {"dates": days,
-            "sales": sales}
+    days, sales, statistics = api_call(data.dates, data.sales, data.ahead)
+    return {data.product:
+            {"dates": days,
+             "sales": sales,
+             "statistics": statistics}}
 
 # uvicorn app_api:app --host 0.0.0.0 --port 10000 (render)
